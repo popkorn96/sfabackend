c106245421075e6767aa3b3801b2faaf18e389be
popkorn96 <mpaloma2014@gmail.com>
2021-05-18 17:55:08 -0500

    created events db table

--- db/migrate/20210518224936_add_events.rb
+++ db/migrate/20210518224936_add_events.rb
@@ -1,6 +1,13 @@
 class AddEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
-      t.
+      t.date :date
+      t.time :time
+      t.string :classification
+      t.string :description
+      t.integer :admin_id
+
+      t.timestamps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_224401) do
+ActiveRecord::Schema.define(version: 2021_05_18_224936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,16 @@ ActiveRecord::Schema.define(version: 2021_05_18_224401) do
 
   create_table "announcements", force: :cascade do |t|
     t.string "title"
+    t.string "classification"
+    t.string "description"
+    t.integer "admin_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
     t.string "classification"
     t.string "description"
     t.integer "admin_id"
